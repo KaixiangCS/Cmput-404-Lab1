0e704e7578dfb76c0556b5bcd09f89fd06828caf
Kaixiang Zhang <kaixiangzhang@Kaixiangs-MacBook-Pro.local>
2020-01-07 10:18:29 -0700

    final

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
 print(requests.__version__)
+vars = requests.get("http://www.google.com")
 var = requests.get("https://raw.githubusercontent.com/KaixiangCS/Cmput-404-Lab1/master/script.py")
 print(var.content)
